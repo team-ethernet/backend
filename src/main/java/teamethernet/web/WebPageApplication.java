794f751abb2726c15380ad2380d8ef9c58bbf751
Anton Bothin <abothin@kth.se>
2019-04-24 10:12:19 +0200

    Created spring project

---
+++ src/main/java/teamethernet/web/WebPageApplication.java
@@ -0,0 +1,13 @@
+package teamethernet.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPageApplication.class, args);
+	}
+
+}
