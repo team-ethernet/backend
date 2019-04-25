7da961abc386e91a47d2af4841eba5bb937690f8
Anton Bothin <abothin@kth.se>
2019-04-25 10:08:30 +0200

    temp commit

--- src/main/java/teamethernet/web/NoiseDataController.java
+++ src/main/java/teamethernet/web/NoiseDataController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -19,14 +21,12 @@ public class NoiseDataController {
     public String getVisualization(Model model) {
         List<NoiseData> noiseData = getNoiseData();
         model.addAttribute("NoiseData", noiseData);
-
         return "visual";
     }
 
-
     @GetMapping(path = "/data")
     public @ResponseBody
-    Iterable<NoiseData> getData(Model model) {
+    Iterable<NoiseData> getData(@RequestParam("startDate") Date date) {
         return noiseDataRepository.findAll();
     }
 
--- src/main/java/teamethernet/web/NoiseDataRepository.java
+++ src/main/java/teamethernet/web/NoiseDataRepository.java
@@ -1,7 +1,19 @@
 package teamethernet.web;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface NoiseDataRepository extends CrudRepository<NoiseData, Integer> {
+import java.util.Date;
+import java.util.List;
+
+public interface NoiseDataRepository extends JpaRepository<NoiseData, Integer> {
+
+    @Query("select row from NoiseData row where row.date <= :date")
+    List<NoiseData> findAllWithDateTimeBefore(@Param("date") Date date);
+
+    @Query("select row from NoiseData row where row.date >= :date")
+    List<NoiseData> findAllWithDateTimeAfter(@Param("date") Date date);
+
 
 }
