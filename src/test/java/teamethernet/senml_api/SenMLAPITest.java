5683cc2ccfa915b757a31a19440d95da48c9d9b7
Isak Olsson <isaak.olsson@gmail.com>
2019-05-13 10:42:32 +0200

    Examples test

--- src/test/java/teamethernet/senml_api/SenMLAPITest.java
+++ src/test/java/teamethernet/senml_api/SenMLAPITest.java
@@ -10,7 +10,29 @@ public class SenMLAPITest {
     }
 
     @Test
-    public void test() {
-
+    public void json_many_parameters() {
+		SenMLAPI = new SenMlAPI.initJsonEncode();
+		Pair bn = new Pair<>(Label.BASE_NAME, "mac:urn:dev:3290329032");
+		Pair v = new Pair<>(Label.VALUE, 30.00);
+		Pair vb = new Pair<>(Label.BOOLEAN_VALUE, false);
+		Pair u = new Pair<>(Label.UNIT, "dB");
+		SenMLAPI.addRecord(bn, v, vb, u);
+		assertTrue(SenMLAPI.endSenML()).isEqualTo("[{\"bn\":\"mac:urn:dev:3290329032\",\"v\":30.000000,\"vb\":false,\"u\":\"dB\"}]");
     }
+
+    @Test
+    public void json_multiple_records() {
+		SenMLAPI = new SenMlAPI.initJsonEncode();
+		Pair bn1 = new Pair<>(Label.BASE_NAME, "mac:urn:dev:3290329032");
+		Pair bn2 = new Pair<>(Label.BASE_NAME, "mac:urn:dev:329032942");
+		Pair bver = new Pair<>(Label.BASE_VERSION, 0);
+		Pair vs = new Pair<>(Label.STRING_VALUE, "hello");
+		Pair ut = new Pair<>(Label.UPDATE_TIME, "30.00");
+
+		SenMLAPI.addRecord(bn1, bver);
+		SenMLAPI.addRecord(bn2, vs, ut);
+		assertTrue(SenMLAPI.endSenML()).isEqualTo("[{\"bn\":\"mac:urn:dev:3290329032\",\"bver\":0},{\"bn\":\"mac:urn:dev:329032942\",\"vs\":\"hello\",\"ut\":\"30.00\"}]");
+    }
+
+
 }
